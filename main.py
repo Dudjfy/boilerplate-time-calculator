1c668b8b9a4f8b13caf255d49130a5887a375abc
R3kt0rN <roma.chirin@gmail.com>
2021-07-19 18:17:45 +0200

    finished new version of time converter, works fine

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from time_calculator import add_time
 from unittest import main
 
 
-print(add_time("11:06 PM", "2:02"))
-print(add_time("11:06 AM", "2:02"))
-print(add_time("1:06 PM", "2:02"))
-print(add_time("1:06 AM", "2:02"))
-
-print(add_time("11:06 PM", "2:02"), 'mOnday')
-print(add_time("11:06 AM", "2:02"), 'mOnday')
-print(add_time("1:06 PM", "2:02"), 'mOnday')
-print(add_time("1:06 AM", "2:02"), 'mOnday')
+# print(add_time("11:06 PM", "2:02"))
+# print(add_time("11:06 AM", "2:02"))
+# print(add_time("1:06 PM", "2:02"))
+# print(add_time("1:06 AM", "2:02"))
+#
+# print(add_time("11:06 PM", "2:02", 'mOnday'))
+# print(add_time("11:06 AM", "2:02", 'mOnday'))
+# print(add_time("1:06 PM", "2:02", 'mOnday'))
+# print(add_time("1:06 AM", "2:02", 'mOnday'))
+# print(add_time("8:16 PM", "466:02", "tuesday"))
 
 # Run unit tests automatically
-# main(module='test_module', exit=False)
+main(module='test_module', exit=False)
